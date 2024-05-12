045a22c64bb65067cde542d860f297679453eae9
maz <7367135+mjbaig@users.noreply.github.com>
2024-05-11 22:07:49 -0700

    SDL Works

--- src/main.c
+++ src/main.c
@@ -11,63 +11,31 @@ int SCREEN_WIDTH = 800;
 
 int SCREEN_HEIGHT = 600;
 
-bool init(void) {
-  bool isSuccess = true;
+int main(void) {
+  SDL_Window *window = NULL;
+
+  SDL_Surface *screenSurface = NULL;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("SDL could not initialize: %s\n", SDL_GetError());
-    isSuccess = false;
-  } else {
-    sdlWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-                                 SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                 SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    fprintf(stderr, "Error initializaing video %s\n", SDL_GetError());
+    return 1;
   }
 
-  if (sdlWindow == NULL) {
-    printf("SDL Window Could Not Be Created: %s\n", SDL_GetError());
-    isSuccess = false;
-  } else {
-    graphicsScreenSurface = SDL_GetWindowSurface(sdlWindow);
+  window = SDL_CreateWindow("sdl-test", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (window == NULL) {
+    fprintf(stderr, "count not create window: %s\n", SDL_GetError());
+    return 1;
   }
 
-  return isSuccess;
-}
-
-bool localMedia(void) {
-  bool isSuccess = true;
-
-  helloWorldSurface = SDL_LoadBMP("");
-
-  if (helloWorldSurface == NULL) {
-    printf("Error making hello world surface: %s\n", SDL_GetError());
-  }
-
-  return isSuccess;
-}
-
-void close(void) {
-  SDL_FreeSurface(helloWorldSurface);
-  helloWorldSurface = NULL;
-
-  SDL_FreeSurface(graphicsScreenSurface);
-  graphicsScreenSurface = NULL;
-
-  SDL_DestroyWindow(sdlWindow);
-  sdlWindow = NULL;
-
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL,
+               SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(2000);
+  SDL_DestroyWindow(window);
   SDL_Quit();
-}
-
-int main(void) {
-  init();
-  localMedia();
-  close();
-
-  if (init()) {
-    if (localMedia()) {
-      SDL_BlitSurface(helloWorldSurface, NULL, graphicsScreenSurface, NULL);
-    }
-  }
-
   return 0;
 }
