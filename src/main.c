b0ff326375e463f200638a14ddaa180f04c2d4c3
maz <7367135+mjbaig@users.noreply.github.com>
2024-05-11 21:22:07 -0700

    update cmake

--- src/main.c
+++ src/main.c
@@ -1,4 +1,81 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-int main() { return 0; }
+SDL_Window *sdlWindow = NULL;
+
+SDL_Surface *graphicsScreenSurface = NULL;
+
+SDL_Surface *helloWorldSurface = NULL;
+
+int SCREEN_WIDTH = 800;
+
+int SCREEN_HEIGHT = 600;
+
+bool init();
+
+bool localMedia();
+
+void close();
+
+int main();
+
+bool init() {
+  bool isSuccess = true;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize: %s\n", SDL_GetError);
+    isSuccess = false;
+  } else {
+    sdlWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+                                 SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                 SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  }
+
+  if (sdlWindow == NULL) {
+    printf("SDL Window Could Not Be Created: %s\n", SDL_GetError);
+    isSuccess = false;
+  } else {
+    graphicsScreenSurface = SDL_GetWindowSurface(sdlWindow);
+  }
+
+  return isSuccess;
+}
+
+bool localMedia() {
+  bool isSuccess = true;
+
+  helloWorldSurface = SDL_LoadBMP("");
+
+  if (helloWorldSurface == NULL) {
+    printf("Error making hello world surface: %s\n", SDL_GetError);
+  }
+
+  return isSuccess;
+}
+
+void close() {
+  SDL_FreeSurface(helloWorldSurface);
+  helloWorldSurface = NULL;
+
+  SDL_FreeSurface(graphicsScreenSurface);
+  graphicsScreenSurface = NULL;
+
+  SDL_DestroyWindow(sdlWindow);
+  sdlWindow = NULL;
+
+  SDL_Quit();
+}
+
+int main() {
+  init();
+  localMedia();
+  close();
+
+  if (init()) {
+    if (localMedia()) {
+      SDL_BlitSurface(helloWorldSurface, NULL, graphicsScreenSurface, NULL);
+    }
+  }
+
+  return 0;
+}
